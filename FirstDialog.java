064412a7314058655b6859963c17a9552f8b88d8
npd632 <nguye.phamduy2006@gmail.com>
2025-10-02 13:36:21 +0700

    Upload project files

---
+++ FirstDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        // Display a dialog box with a message
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
---
+++ HelloNameDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class HelloNameDialog {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Please Enter Your Name:");
+        JOptionPane.showMessageDialog(null, "Hello " + name + "!");
+        System.exit(0);
+    }
+}
